f8a4b6e3822ec9ead3350e6eed988b45bc4026b0
mallikaph013 <mallikaph013@gmail.com>
2026-01-24 15:45:00 +0000

    Update navigation and news links

--- obj/Debug/net9.0/myapp.AssemblyInfo.cs
+++ obj/Debug/net9.0/myapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+498f5acf0d70346bdb2e513a2696994d15544110")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+340bf8813110ae9c05bd8d149416fba7c1821796")]
 [assembly: System.Reflection.AssemblyProductAttribute("myapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
